e16c48fa9dca9c29ad769c8b44df5371df9126a8
akiyori <akiyori@users.noreply.github.com>
2018-06-02 17:43:51 +0900

    マップベース探索Ver

--- VTC2/character_manager.cpp
+++ VTC2/character_manager.cpp
@@ -1,13 +1,19 @@
 #include "character_manager.h"
 #include <thread>
 #include "stopwatch.h"
+#include <string> 
+#include "spdlog/spdlog.h"
 
 CharacterManager::CharacterManager()
 {
-	for (int i = 0; i < 100; i++) {
+	int count = 100;
+	characters.reserve(count *2);
+	for (int i = 0; i < count; i++) {
 		characters.push_back(new Character(0));
 		characters.push_back(new Character(1));
 	}
+
+	map = std::vector<std::vector<int>>(MAP_SIZE, std::vector<int>(MAP_SIZE, -1));
 }
 
 CharacterManager::~CharacterManager()
@@ -17,26 +23,54 @@ CharacterManager::~CharacterManager()
 void CharacterManager::Update(double timeTotal, double timeDelta)
 {
 	{
-		StopWatch<std::chrono::milliseconds> sw;
+		StopWatch<std::chrono::milliseconds> sw("reset map");
 
-		for (Character* character : characters) {
+		for (auto& row : map) {
+			std::fill(row.begin(), row.end(), -1);
+		}
+	}
+	{
+		StopWatch<std::chrono::milliseconds> sw("create map");
+
+		for (size_t i = 0, n = characters.size(); i < n; ++i) {
+			auto* character = characters[i];
+			map[character->position.x][character->position.y] = i;
+		}
+	}
+	{
+		StopWatch<std::chrono::milliseconds> sw("target process");
+		//std::string r = "";
+		//std::string c = "";
+		//auto logger = spdlog::get("async_file_logger");
+		//for (auto row : map) {
+		//	for (auto cell : row) {
+		//		r.append(std::to_string(cell)).push_back(',');
+		//	}
+		//	logger->info(r);
+		//	r = "";
+		//}
+		
+		for (Character* character : characters)
+		{
 			std::vector<Character*> targetInSight = {};
-			for (Character* target : characters) {
-				if (character->sightRange >= Point::Distance(character->position, target->position)) {
-					targetInSight.push_back(target);
+			targetInSight.reserve(64);
+			int startX = character->position.x - character->sightRange / 2;
+			int endX = character->position.x + character->sightRange / 2;
+			int startY = character->position.y - character->sightRange / 2;
+			int endY = character->position.y + character->sightRange / 2;
+			if (startX < 0) startX = 0;
+			if (endX >= 1000) endX = 1000 - 1;
+			if (startY < 0) startY = 0;
+			if (endY >= 1000) endY = 1000 - 1;
+			for (int x = startX; x < endX; ++x)
+			{
+				for (int y = startY; y < endY; ++y)
+				{
+					if(map[x][y] > 0)
+						targetInSight.push_back(characters[map[x][y]]);
 				}
 			}
 			character->Action(targetInSight, timeDelta);
-
-			//std::thread t1([&targetInSight](std::vector<Character*> characters, Character* character) {
-			//	for (Character* target : characters) {
-			//		if (character->sightRange >= Point::Distance(character->position, target->position)) {
-			//			targetInSight.push_back(target);
-			//		}
-			//	}
-			//	character->Action(targetInSight);
-			//}, characters, character);
-			//t1.join();
 		}
 	}
 }
--- VTC2/character_manager.h
+++ VTC2/character_manager.h
@@ -5,11 +5,12 @@
 #include "graphics.h"
 #include "point.h"
 
-
 class CharacterManager
 {
 public:
+	static const int MAP_SIZE = 1000;
 	std::vector<Character*> characters;
+	std::vector<std::vector<int>> map;
 
 	CharacterManager();
 	~CharacterManager();

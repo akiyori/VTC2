a4b7fdce73fef610e3e11b8dcce41ec13fe7d619
akiyori <akiyori@users.noreply.github.com>
2018-06-04 21:16:44 +0900

    GetPartyPositionをprivateに変更

--- VTC2/party.h
+++ VTC2/party.h
@@ -2,26 +2,8 @@
 #include "character_manager.h"
 
 class Party : public CharacterManager{
-public:
-	int affiliationId;
 
-	Party(int affiliationId, int id, const std::string &name) : CharacterManager(id, name)
-	{
-		this->affiliationId = affiliationId;
-		destination = Point((id+1) * 100, (1 - affiliationId) * 600 + 100);
-
-		int count = 8;
-		members.reserve(count);
-		for (int i = 0; i < count; i++) {
-			auto character = new Character(affiliationId, id);
-			character->position = GetPartyPosition(Point((id + 1) * 100, affiliationId * 600 + 100), destination, i);
-			character->destination = destination;
-			allCharacters.push_back(character);
-			members.push_back(character);
-		}
-	}
-
-	Point GetPartyPosition(Point base, Point target, int index){
+	Point GetPartyPosition(Point base, Point target, int index) {
 		float distanceRate = 5;
 		auto dir = Point::Direction(base, target);
 		Point diff = Point(0, 0);
@@ -54,5 +36,24 @@ public:
 			return Point(0, 0);
 		}
 		return base + diff * distanceRate;
+	}
+public:
+	int affiliationId;
+
+	Party(int affiliationId, int id, const std::string &name, Point basePosition) : CharacterManager(id, name)
+	{
+		this->affiliationId = affiliationId;
+		destination.x = basePosition.x;
+		destination.y = basePosition.y * (affiliationId>0? -1:6);
+
+		int count = 8;
+		members.reserve(count);
+		for (int i = 0; i < count; i++) {
+			auto character = new Character(affiliationId, id);
+			character->position = GetPartyPosition(basePosition, destination, i);
+			character->destination = destination;
+			allCharacters.push_back(character);
+			members.push_back(character);
+		}
 	}
 };

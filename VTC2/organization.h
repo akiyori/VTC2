dc3c5139ce7a4a77ec18e6e1879e6255ec6c1dc6
akiyori <akiyori@users.noreply.github.com>
2018-06-04 21:18:02 +0900

    陣形追加

--- VTC2/organization.h
+++ VTC2/organization.h
@@ -3,15 +3,64 @@
 #include "party.h"
 
 class Organization : public CharacterManager {
+
+	Point GetPartyPosition(Point base, Point target, int index) {
+		float distanceRate = 100;
+		auto dir = Point::Direction(base, target);
+		Point diff = Point(0, 0);
+		switch (index)
+		{
+		case 0:
+			break;
+		case 1:
+			diff = Point::Rotate(dir, -90);
+			break;
+		case 2:
+			diff = Point::Rotate(dir, 90);
+			break;
+		case 3:
+			diff = Point::Rotate(dir, -30);
+			break;
+		case 4:
+			diff = Point::Rotate(dir, 30);
+			break;
+		case 5:
+			diff = Point::Rotate(dir, -90) * 2;
+			break;
+		case 6:
+			diff = Point::Rotate(dir, 90) * 2;
+			break;
+		case 7:
+			diff = Point::Rotate(dir, -60) * 1.732;
+			break;
+		case 8:
+			diff = Point::Rotate(dir, 60) * 1.732;
+			break;
+		case 9:
+			diff = Point::Rotate(dir, 0) * 1.732;
+			break;
+		case 10:
+			diff = Point::Rotate(dir, -30) * 2;
+			break;
+		case 11:
+			diff = Point::Rotate(dir, 30) * 2;
+			break;
+		default:
+			return Point(0, 0);
+		}
+		return base + diff * distanceRate;
+	}
 public:
 	std::vector<Party*> parties;
 
 	Organization(int id, const std::string &name) : CharacterManager(id, name) 
 	{
-		int count = 16;
+		int count = 12;
 		parties.reserve(count);
+		auto base = Point(1280/2, 100+id*600);
+		destination = Point(1280 / 2, 100 + (1-id) * 600);
 		for (int i = 0; i < count; i++) {
-			auto party = new Party(id, i, "party" + std::to_string(i));
+			auto party = new Party(id, i, "party" + std::to_string(i), GetPartyPosition(base, destination, i));
 			parties.push_back(party);
 		}
 	}
